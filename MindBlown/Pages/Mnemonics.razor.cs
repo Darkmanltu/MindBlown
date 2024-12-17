ad7820d983b92165dc36fbebc16c647d4a9900e6
Darkmanltu <darkermec@gmail.com>
2024-12-17 20:32:17 +0200

    more deletes and warnings fix

--- MindBlown/Pages/Mnemonics.razor.cs
+++ MindBlown/Pages/Mnemonics.razor.cs
@@ -21,8 +21,6 @@ namespace MindBlown.Pages
         public required IActiveUserClient ActiveUserClient { get; set; }
         [Inject]
         public required IAuthService AuthService { get; set; }
-
-        // private TimedRemovalService TimedRemovalService { get; set; } = default!;
         public MnemonicsType Model { get; set; } = new MnemonicsType();
 
         public int ActiveUserCount {get; set;}
@@ -61,7 +59,6 @@ namespace MindBlown.Pages
             // Retrieve user ID from session storage or generate a new one if it doesn't exist
             userId = await JS.InvokeAsync<Guid>("sessionStorage.getItem", "userId");
 
-            // Console.WriteLine("User ID: " + userId);
             // Add the user to ActiveUserClient
             bool isUnique = await ActiveUserClient.IsSessionIdUniqueAsync(userId);
 
@@ -70,9 +67,6 @@ namespace MindBlown.Pages
             {
                 // Add the user to ActiveUserClient only if the sessionId is unique
                 await ActiveUserClient.AddUserAsync(userId);
-
-                // Update the active user count
-
             }
 
 
@@ -80,9 +74,7 @@ namespace MindBlown.Pages
 
             await ActiveUserClient.RemoveInnactive();
             var activeUserDict = await ActiveUserClient.GetDictionary();
-            //ActiveUserCount = await ActiveUserClient.GetActiveUserCountAsync();
             ActiveUserCount = await ActiveUserClient.GetActiveUserCountAsync(activeUserDict);
-            //await ActiveUserClient.RemoveUserAsync(userId);
 
 
             _timer = new Timer(async async =>
@@ -113,7 +105,6 @@ namespace MindBlown.Pages
         {
             // Retrieve the active user count
             var activeUserCountCheck = await ActiveUserClient.GetActiveUserCountAsync(await ActiveUserClient.GetDictionary());
-            // Console.WriteLine($"Active user recounter: {activeUserCountCheck}");
 
             // Update the state if the count has changed
             if (ActiveUserCount != activeUserCountCheck)
@@ -148,7 +139,7 @@ namespace MindBlown.Pages
                     }
                 }
 
-                // mnemonicAlreadyExists = existingMnemonics.Any(m => m.HelperText == Model.HelperText);
+             
                 mnemonicAlreadyExists = existingMnemonics.Where(m => m.HelperText == Model.HelperText).Count() > 0;
 
                 if (mnemonicAlreadyExists)
@@ -184,7 +175,7 @@ namespace MindBlown.Pages
                     Message = ex.Message,
                     Details = ex.ToString()
                 });
-                // await ShowErrorMessage(ex.Message);
+                
                 Model = new MnemonicsType();
                 StateHasChanged();
                 await Task.Delay(3000);
@@ -204,7 +195,7 @@ namespace MindBlown.Pages
         public async Task LoadMnemonics()
         {
             // Get mnemonics from database. Returns a list of MnemonicsType
-            // mnemonicsList = await MnemonicService.GetMnemonicsAsync() ?? new List<MnemonicsType>();
+            
             if(await AuthService.IsUserLoggedInAsync())
             {    
                 var username = await AuthService.GetUsername();
@@ -264,7 +255,6 @@ namespace MindBlown.Pages
             catch (Exception )
             {   
                 // throws an exception but still executes it fine idk why
-                //Console.WriteLine($"Error during Dispose: {ex.Message}");
             }
 
 
@@ -277,10 +267,7 @@ namespace MindBlown.Pages
         {
             // Perform async cleanup
             var userId = await JS.InvokeAsync<Guid>("sessionStorage.getItem", "userId");
-            // unnecasary
-            // await ActiveUserClient.RemoveUserAsync(userId);
             var activeUserDict = await ActiveUserClient.GetDictionary();
-            //ActiveUserCount = await ActiveUserClient.GetActiveUserCountAsync();
             ActiveUserCount = await ActiveUserClient.GetActiveUserCountAsync(activeUserDict);
         }
 
